e81863ac5136c40c876cca5c0287dc4518fae3c4
Shobhit Kasliwal <shobhit.kasliwal@onbe.com>
2021-08-17 16:54:35 -0500

    updates for basic logging

---
+++ src/BcFinalMovieCatalogSvc/Features/Logging/BcFinalMovieCatalogSvcMessageTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BcFinalMovieCatalogSvc.Features.Logging
+{
+    public class BcFinalMovieCatalogSvcMessageTemplates 
+    {
+        public const string saved_COUNT_entities_memory_store = "Saved {count} entities to in-memory store.";
+    }
+}
